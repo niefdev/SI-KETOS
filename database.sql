8521806ce8ed4e0c5b7cdd2e7fc912bb581dd242
Alief Abdur Rahman Salam <88725935+niefdev@users.noreply.github.com>
2024-10-11 08:40:09 +0700

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,61 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+SET NAMES utf8mb4;
+
+CREATE TABLE `foto` (
+  `id` tinyint(1) NOT NULL,
+  `foto` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `kandidat` (
+  `id` tinyint(1) NOT NULL,
+  `nama` varchar(128) NOT NULL,
+  `deskripsi` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `pemilih` (
+  `id` smallint(5) NOT NULL,
+  `kandidat` tinyint(1) NOT NULL,
+  `waktu` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) UNSIGNED NOT NULL,
+  `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `variabel` (
+  `key` varchar(5) NOT NULL,
+  `value` varchar(8) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `variabel` (`key`, `value`) VALUES
+('phase', '1'),
+('seed', 'abc@1234'),
+('voter', '100');
+
+ALTER TABLE `foto`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `kandidat`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `pemilih`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`session_id`);
+
+ALTER TABLE `variabel`
+  ADD PRIMARY KEY (`key`);
+
+ALTER TABLE `foto`
+  MODIFY `id` tinyint(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+ALTER TABLE `kandidat`
+  MODIFY `id` tinyint(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+COMMIT;
